7b67cef9c1fffc1db0e0b43db9b91dd9527c9952
vanadium23 <chernoffivan@gmail.com>
2019-06-30 14:00:29 +0300

    [Feat] task 5 done in ud905

--- lib/category.dart
+++ lib/category.dart
@@ -99,12 +99,11 @@ class Category extends StatelessWidget {
               // See https://www.dartlang.org/guides/language/effective-dart/usage#do-use-collection-literals-when-possible
               children: [
                 Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Icon(
-                    iconLocation,
-                    size: 60.0,
-                  ),
-                ),
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(
+                      iconLocation,
+                      size: 60.0,
+                    )),
                 Center(
                   child: Text(
                     name,
